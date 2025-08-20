e376094f4b9dfc22b37bccafa182109deee23e66
Amit-Bhandari0 <amit00bhandari@gmail.com>
2025-08-20 19:05:54 +0545

    Removed Postgre SQL and added MySQL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,12 +55,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'ssl': {
+                'ca': '/etc/ssl/certs/ca-certificates.crt',
+            }
+        }
     }
 }
 
